b13fe9fec8b330f44c4acb9fc07f536e700c7b07
Sam Vipond <sammy.potato@gmail.com>
2020-02-01 13:35:21 -0500

    move state to parent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import MultiSelect from './MultiSelect';
 
@@ -38,15 +38,121 @@ const globalShiftList = [
 ];
 
 
-function App() {
+class App extends Component {
 
-  return (
-    <div className='app'>
-      <h1>Demo</h1>
-      <MultiSelect
-        globalShiftList={globalShiftList} />
-    </div>
-  );
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			allShifts: globalShiftList,
+			available: globalShiftList,
+			unavailable: [],
+			selected: [],
+		}
+	}
+
+	selectShift = (option) => {
+    if(!option.available) {
+      console.log('shift not available');
+      return;
+    }
+
+		const newSelected = [...this.state.selected, option];
+
+    const { all, validFormatted, notAvailableFormatted } = this.getAvailableShifts(newSelected, globalShiftList);
+
+    this.setState({
+      allShifts: all,
+      available: validFormatted,
+      unavailable: notAvailableFormatted,
+      selected: newSelected.sort((a, b) => {
+        return a.start - b.start;
+      }),
+    });
+
+  }
+
+	getAvailableShifts = (userShifts, globalShiftList) => {
+
+    const invalid = [];
+
+    userShifts.forEach((uShift) => {
+      const uStart = parseInt(uShift.start);
+      const uEnd = parseInt(uShift.end);
+      globalShiftList.forEach((gShift) => {
+        const gStart = parseInt(gShift.start);
+        const gEnd = parseInt(gShift.end);
+
+        if (invalid.includes(gShift)) return;
+
+        if (gStart >= uStart && gEnd <= uEnd) {
+          invalid.push(gShift);
+        }
+
+        if (gEnd > uStart && gEnd <= uEnd) {
+          invalid.push(gShift);
+        }
+
+        if (gStart < uStart && gEnd > uStart) {
+          invalid.push(gShift);
+        }
+
+        if (gStart >= uStart && gStart < uEnd) {
+          invalid.push(gShift);
+        }
+
+      });
+    });
+
+    const valid = globalShiftList.filter((shift) => !invalid.includes(shift));
+    const notAvailable = globalShiftList.filter((shift) => invalid.includes(shift));
+
+    const validFormatted = valid.map((shift) => ({
+      available: true,
+      ...shift,
+    }));
+
+    const notAvailableFormatted = notAvailable.map((shift) => ({
+      available: false,
+      ...shift,
+    }));
+
+    const all = [...validFormatted, ...notAvailableFormatted].sort((a, b) => {
+      return a.start - b.start;
+		});
+
+    return { all, validFormatted, notAvailableFormatted };
+
+	};
+
+	reset = () => {
+    const { all, validFormatted, notAvailableFormatted } = this.getAvailableShifts([], globalShiftList);
+    this.setState({
+      allShifts: all,
+      available: validFormatted,
+      unavailable: notAvailableFormatted,
+      selected: [],
+		});
+	}
+
+	UNSAFE_componentWillMount() {
+    this.reset();
+  }
+
+	render() {
+		return (
+			<div className='app'>
+				<h1>Demo</h1>
+				<MultiSelect
+					selected={this.state.selected}
+					available={this.state.available}
+					unavailable={this.state.unavailable}
+					allShifts={this.state.allShifts}
+					onReset={this.reset}
+					onSelectShift={this.selectShift} />
+			</div>
+		);
+	}
 }
 
 export default App;
